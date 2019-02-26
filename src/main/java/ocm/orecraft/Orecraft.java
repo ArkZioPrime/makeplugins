898a4733a227411b709092d04543d93e54b93f73
LXSN <www.159251664@qq.com>
2019-02-26 22:17:54 +0800

    how to build a plugin

---
+++ src/main/java/ocm/orecraft/Orecraft.java
@@ -0,0 +1,17 @@
+package ocm.orecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Orecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
